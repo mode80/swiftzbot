f80f63a586efec2920485ee9ed74c91bb27aa8ef
mode80 <mode80@users.noreply.github.com>
2022-12-13 11:45:21 -0700

    first pass translation static compiles

--- Package.swift
+++ Package.swift
@@ -8,13 +8,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+            .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"), // Algorithms
+            .package(url: "https://github.com/ebraraktas/swift-tqdm", from: "0.1.2"), // tqdm 
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "swiftzbot",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Tqdm", package: "swift-tqdm"),
+             ]),
         .testTarget(
             name: "swiftzbotTests",
             dependencies: ["swiftzbot"]),
--- Sources/swiftzbot/swiftzbot.swift
+++
@@ -1,8 +0,0 @@
-@main
-public struct swiftzbot {
-    public private(set) var text = "Hello, World!"
-
-    public static func main() {
-        print(swiftzbot().text)
-    }
-}
